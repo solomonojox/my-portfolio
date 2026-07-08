61896f14fe299879aef52a19b32e7727f8550c44
Akpa Solomon <solomonakpas@gmail.com>
2026-07-08 22:01:12 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
-import { redirect } from "next/navigation"
+// import { getServerSession } from "next-auth"
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+// import { redirect } from "next/navigation"
 import HomePageComponent from '@/components/landing';
 
 
 export default async function HomePage() {
-  const session = await getServerSession(authOptions)
+  // const session = await getServerSession(authOptions)
 
-  if (!session) {
-    redirect("/login")
-  }
+  // if (!session) {
+  //   redirect("/login")
+  // }
 
   return <HomePageComponent />
 }
